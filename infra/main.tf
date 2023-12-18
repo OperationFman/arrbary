2beda24e417e94358fa17cd3d57ec7b1efe556a8
OperationFman <franklin.m.moon@gmail.com>
2023-12-18 17:42:42 +0700

    :alembic: Remove k8s Bootstrap from tf workflow

--- infra/main.tf
+++ infra/main.tf
@@ -4,22 +4,6 @@ resource "aws_instance" "demo-ec2-instance" {
 
     vpc_security_group_ids = [aws_security_group.ssh-group.id]
 
-    user_data = "${file("k8s-bootstrap.sh")}"
+    # user_data = "${file("k8s-bootstrap.sh")}"
 }
 
-resource "aws_security_group" "ssh-group" {
-    name = "ssh"
-    ingress { 
-        cidr_blocks = ["0.0.0.0/0"]
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
