8bf5f61ad6d44c386200b07857f2b19371b103f4
OperationFman <franklin.m.moon@gmail.com>
2023-12-19 12:31:15 +0700

    :fire: Remove broken cluster config

--- infra/k8s_bootstrap.sh
+++ infra/k8s_bootstrap.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Install Docker
 sudo yum update -y
-sudo amazon-linux-extras install -y docker
+sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
@@ -13,15 +13,3 @@ sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 sudo curl -s https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
-# Init cluster
-k3d cluster create mycluster -p "8900:30080@agent:0" -p "8901:30081@agent:0" -p "8902:30082@agent:0" --agents 2
-
-# Install Rancher
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-helm install rancher rancher-latest/rancher \
-   --namespace cattle-system \
-   --create-namespace \
-   --set ingress.enabled=false \
-   --set tls=external \
-   --set replicas=1
--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "demo-ec2-instance" {
-    ami = "ami-06fdec94cc3067ad1"
+    ami = "ami-09eebd0b9bd845bf1"
     instance_type = "t2.micro"
 
     vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
-    # user_data = "${file("k8s_bootstrap.sh")}"
+    user_data = "${file("k8s_bootstrap.sh")}"
 }
-
