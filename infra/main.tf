8f4679009d04899aae465518eb9c0d03ef01cf16
OperationFman <franklin.m.moon@gmail.com>
2023-12-19 10:07:07 +0700

    :bug: Mismatched ssh name

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "demo-ec2-instance" {
     ami = "ami-06fdec94cc3067ad1"
     instance_type = "t2.micro"
 
-    vpc_security_group_ids = [aws_security_group.ssh-group.id]
+    vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
     # user_data = "${file("k8s_bootstrap.sh")}"
 }
