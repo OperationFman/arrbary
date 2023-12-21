c2eec4bc053843d75471a389774f81b07ea2a150
OperationFman <franklin.m.moon@gmail.com>
2023-12-21 11:27:11 +0700

    :sparkles: Add k8s-admin security group to VPC

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,10 @@ resource "aws_instance" "demo-ec2-instance" {
     ami = "ami-09eebd0b9bd845bf1"
     instance_type = "t2.micro"
 
-    vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+    vpc_security_group_ids = [
+        aws_security_group.allow-ssh.id,
+        aws_security_group.k8s-admin.id,
+    ]
 
     user_data = "${file("user_data.sh")}"
 }
